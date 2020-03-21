494cf5447d02defa107b42b942310e3e856b96da
Tiffany McLean <tamclean324@gmail.com>
2020-03-20 23:55:32 -0400

    completes tuneup

--- tuneup.py
+++ tuneup.py
@@ -2,19 +2,35 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "Tiffany McLean with the help of a lot of Google-fu and Janell's & Karen's awesome handout that covered the profile portion "
 
 import cProfile
 import pstats
 import functools
+import timeit
 
 
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
+    @functools.wraps(func)
+    def profile_wrapper(*args, **kwargs):
+        performance_object = cProfile.Profile()
+        performance_object.enable()
+        result = func(*args, **kwargs)
+        performance_object.disable
+
+        get_stats_obj = pstats.Stats(performance_object)
+        get_stats_obj.strip_dirs()
+        get_stats_obj.sort_stats('cumulative')
+        get_stats_obj.print_stats()
+
+        return result
+
+    return profile_wrapper
+
     # You need to understand how decorators are constructed and used.
     # Be sure to review the lesson material on decorators, they are used
     # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
 
 
 def read_movies(src):
@@ -24,28 +40,38 @@ def read_movies(src):
         return f.read().splitlines()
 
 
-def is_duplicate(title, movies):
-    """returns True if title is within movies list"""
-    for movie in movies:
-        if movie.lower() == title.lower():
-            return True
-    return False
-
-
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
+
+    # create an object to hold the movies
+    movie_obj = {}
     movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
-    return duplicates
+
+    for movie in movies:
+        if movie_obj.get(movie):
+            # if it's already there we need to increment it
+            movie_obj[movie] += 1
+        else:
+            # if the movie is already there we need to add it
+            movie_obj[movie] = 1
+
+    return [k for k, v in movie_obj.items() if v > 1]
 
 
 def timeit_helper():
-    """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    """
+    Part A:  Obtain some profiling measurements using timeit
+
+    Measure the total amt of CPU time required to run the main function
+
+    so main would be stmt and it needs to be imported as part of the set up
+
+    """
+
+    t = timeit.Timer(stmt="main()", setup="from __main__ import main")
+    results = min(t.repeat(repeat=7, number=5)) / 5
+    print("Best time across 7 repeats of 5 runs per repeat " + str(results) + " sec")
 
 
 def main():
